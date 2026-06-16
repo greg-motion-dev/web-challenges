17603a57b6f5852ede08399000ca8de12243328b
Greg Telakis <greg.telakis@gmail.com>
2026-06-16 16:16:59 +0200

    More challenges for JavaScript basics have been completed and added.

---
+++ javascript/js-basics-and-variables_social-media-post/index.js
@@ -0,0 +1,36 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a constant variable for each data point listed below:
+	- a title for the post
+	- text content for the post
+	- the number of likes the post has received
+	- the user who created the post
+	- a boolean called `isReported` to indicate whether the post has been reported
+*/
+
+// --v-- write your code here --v--
+
+const postTitle = "This is a title.";
+const postContent = "Here is the descriptive text for the social media post.";
+const postLikes = 0;
+const postUser = "Jane";
+const isReported = true;
+
+
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. Then increase the number of likes by one and log the updated like count. Modify your code from step 1 if necessary.
+*/
+
+// --v-- write your code here --v--
+
+const postTitle = "This is a title.";
+const postContent = "Here is the descriptive text for the social media post.";
+let postLikes = 0;
+postLikes++;
+const postUser = "Jane";
+const isReported = true; // since the boolean should be switchables, let isReported = true, would make more sense, unless "true" should never change.
+
+// --^-- write your code here --^--
