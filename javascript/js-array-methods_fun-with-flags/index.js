05dc720053683af367cb54cef1e9106c1522d526
Greg Telakis <greg.telakis@gmail.com>
2026-07-02 22:14:55 +0200

    Finished 6 out of 6 JS Loops challenges.

---
+++ javascript/js-array-methods_fun-with-flags/index.js
@@ -0,0 +1,28 @@
+import { countries } from "./utils/countries.js";
+import { Country } from "./components/Country/Country.js";
+
+const container = document.querySelector('[data-js="card-container"]');
+const queryInput = document.querySelector('[data-js="query-input"]');
+
+queryInput.addEventListener("input", (event) => {
+  container.innerHTML = "";
+
+  const searchString = event.target.value;
+
+  const foundCountry = countries.find(country => {country.name.startsWith(searchString)});
+
+  const foundCountries = countries.filter(country => {return country.name.startsWith(searchString).toLowerCase()});
+
+  
+  if (foundCountry) {
+    const countryElement = Country(foundCountry);
+    container.append(countryElement);
+  }
+
+  if (foundCountries) {
+    foundCountries.forEach(country => {
+      const countryElemet  = Country(country);
+      container.append(countryElement)
+    })
+  }
+});
