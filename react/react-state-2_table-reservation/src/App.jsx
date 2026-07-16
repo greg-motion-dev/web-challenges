042526997942b4eb9f3b3c21eff362fa6273b401
Greg Telakis <greg.telakis@gmail.com>
2026-07-16 22:23:00 +0200

    Finished 9 out of 11 challenges for React 2 & 3.

---
+++ react/react-state-2_table-reservation/src/App.jsx
@@ -0,0 +1,22 @@
+import Counter from "./components/Counter";
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [people, setPeople] = useState(0);
+
+  function handleIncrement() {
+    setPeople(people + 1);
+  }
+  function handleDecrement() {
+    setPeople(people - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>Place a Table Reservation</h1>
+      <Counter onIncrement={handleIncrement} onDecrement={handleDecrement} />
+      <p>You are going to reserve a table for {people} people.</p>
+    </div>
+  );
+}
